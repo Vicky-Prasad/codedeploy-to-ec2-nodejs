44cb0799eae824ead365916da3e6c45f1d705885
Vicky Prasad <79387867+Vicky-Prasad@users.noreply.github.com>
2024-06-23 10:53:58 -0600

    Update applicationstart.sh

--- applicationstart.sh
+++ applicationstart.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+#!/bin/bash
+sudo apt-get -y update
+
+# INSTALL NODEJS - ONLY FOR UBUNTU
+sudo apt install nodejs -y
+
+# INSTALL NPM & PM2 - ONLY FOR UBUNTU
+sudo apt install npm -y
+sudo npm install pm2@latest -g
 
 cd /home/ubuntu
 pm2 -f start server.js
