b17ba7b0248996d7553e5a52e07fe3cdda40b6ad
Vicky Prasad <79387867+Vicky-Prasad@users.noreply.github.com>
2024-06-23 10:47:57 -0600

    Update afterinstall.sh

--- afterinstall.sh
+++ afterinstall.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
+# Ensure the environment is updated and Node.js/npm are installed
+echo "Updating server and installing Node.js and npm"
+sudo apt -y update
+sudo apt install -y nodejs
+sudo apt install -y npm
+
+# Verify the installation
+node -v
+npm -v
+
+# Install npm dependencies
+echo "Installing npm dependencies..."
 cd /home/ubuntu
-npm -f install
+npm install
+
+echo "After install script completed."
